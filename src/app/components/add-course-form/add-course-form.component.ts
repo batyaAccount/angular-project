144d83f8b07add9523abf17cb7f83fc4bf1e2301
user1 <user1@DESKTOP-SSNMLFD>
2025-03-09 17:24:14 +0200

    add-remove-lessons

--- src/app/components/add-course-form/add-course-form.component.ts
+++ src/app/components/add-course-form/add-course-form.component.ts
@@ -12,8 +12,8 @@ import { Lessons } from "../../../Lessons";
 
 @Component({
   selector: 'app-add-course-form',
-  standalone:true,
-  imports: [ReactiveFormsModule, MatFormFieldModule, MatInputModule, MatSelectModule,MatButtonModule],
+  standalone: true,
+  imports: [ReactiveFormsModule, MatFormFieldModule, MatInputModule, MatSelectModule, MatButtonModule],
   templateUrl: './add-course-form.component.html',
   styleUrl: './add-course-form.component.css'
 })
@@ -27,9 +27,12 @@ export class AddCourseFormComponent {
       });
     }
   }
+  
+  private isSubmitted: boolean = false;
+
   course: Partial<Courses> | undefined
   courseId: string = '0'
-
+  tmp: Partial<Lessons>[] = []
   addCourseForm = new FormGroup({
     title: new FormControl<string>('', [Validators.required]),
     description: new FormControl<string>('', [Validators.required]),
@@ -44,6 +47,9 @@ export class AddCourseFormComponent {
     });
     this.coursesService.getLessonsInCourses(course.id).subscribe((less) => {
       less.forEach(l => {
+        this.tmp.push(l)
+        this.coursesService.removeLessonFromCourse(course.id as string, l.id as string).subscribe(s => {
+        })
         this.lessons.push(new FormGroup({
           title: new FormControl(l.title, Validators.required),
           content: new FormControl(l.content, Validators.required)
@@ -51,59 +57,65 @@ export class AddCourseFormComponent {
       });
     })
     this.lessons.clear();
-    
 
-}
-  get lessons(): FormArray {
-  return this.addCourseForm.get('lessons') as FormArray;
-}
-addLesson(): void {
-  this.lessons.push(new FormGroup({
-    title: new FormControl('', Validators.required),
-    content: new FormControl('', Validators.required)
-  }));
-}
-removeLesson(index: number): void {
-  this.lessons.removeAt(index);
-}
-sent() {
-  this.user.getTeacerById(this.addCourseForm.value.teacherId as string).subscribe(res => {
 
-    if (this.course == undefined) {
-      this.addCourse();
-    } else {
-      this.updateCourse(this.addCourseForm.value as Partial<Courses>, this.course.id);
-    }
-    this.router.navigate(['/home/courses-page']);
-
-  }, error => {
-    alert("The teacherId is not valid. Please try again.");
-    this.router.navigate(['/home/courses-page']);
-
-  });
-}
-addCourse() {
-  var cour = {
-    title: this.addCourseForm.value.title ?? undefined,
-    description: this.addCourseForm.value.description ?? undefined,
-    teacherId: this.addCourseForm.value.teacherId ?? undefined,
-    lessons: this.lessons.controls.map(control => control.value)
-  } as Partial<Courses>
-  this.coursesService.addCourses(cour).subscribe(
-    r => {
-      this.courseId = r.courseId;
-      for (let index = 0; index < this.lessons.length; index++) {
-        this.coursesService.addLessonToCourse(this.lessons.at(index).value as Partial<Lessons>, this.courseId).subscribe(r => { },
-          err => console.log(err))
-      }
-    },
-    error => alert("error at adding course"))
-
-}
-updateCourse(course: Partial<Courses>, id: string | undefined) {
-  this.coursesService.updateCourse(course, id).subscribe(r => { }, error => alert("You are not allowed to update"))
-  for (let index = 0; index < this.lessons.length; index++) {
-    this.coursesService.addLessonToCourse(this.lessons.at(index).value as Partial<Lessons>, id ? id : '-1').subscribe()
   }
-}
+  get lessons(): FormArray {
+    return this.addCourseForm.get('lessons') as FormArray;
+  }
+  addLesson(): void {
+    this.lessons.push(new FormGroup({
+      title: new FormControl('', Validators.required),
+      content: new FormControl('', Validators.required)
+    }));
+  }
+  removeLesson(index: number): void {
+    this.lessons.removeAt(index);
+    let l: Partial<Lessons>[] = []
+    for (let i = 0; i < this.tmp.length && index != i; i++) {
+      l.push(this.tmp[index]);
+
+    }
+  }
+  sent() {
+    this.user.getTeacerById(this.addCourseForm.value.teacherId as string).subscribe(res => {
+      this.isSubmitted = true
+      if (this.course == undefined) {
+        this.addCourse();
+      } else {
+        this.updateCourse(this.addCourseForm.value as Partial<Courses>, this.course.id);
+      }
+      this.router.navigate(['/home/courses-page']);
+
+    }, error => {
+
+      alert("The teacherId is not valid. Please try again.");
+      this.router.navigate(['/home/courses-page']);
+
+    });
+  }
+  addCourse() {
+    var cour = {
+      title: this.addCourseForm.value.title ?? undefined,
+      description: this.addCourseForm.value.description ?? undefined,
+      teacherId: this.addCourseForm.value.teacherId ?? undefined,
+      lessons: this.lessons.controls.map(control => control.value)
+    } as Partial<Courses>
+    this.coursesService.addCourses(cour).subscribe(
+      r => {
+        this.courseId = r.courseId;
+        for (let index = 0; index < this.lessons.length; index++) {
+          this.coursesService.addLessonToCourse(this.lessons.at(index).value as Partial<Lessons>, this.courseId).subscribe(r => { },
+            err => console.log(err))
+        }
+      },
+      error => alert("error at adding course"))
+
+  }
+  updateCourse(course: Partial<Courses>, id: string | undefined) {
+    this.coursesService.updateCourse(course, id).subscribe(r => { }, error => alert("You are not allowed to update"))
+    for (let index = 0; index < this.lessons.length; index++) {
+      this.coursesService.addLessonToCourse(this.lessons.at(index).value as Partial<Lessons>, id ? id : '-1').subscribe()
+    }
+  }
 }
