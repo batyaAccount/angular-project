1090fac3187fefa6d14ef46023a995dfc8a57a18
user1 <user1@DESKTOP-SSNMLFD>
2025-03-11 00:32:58 +0200

    last2

--- src/app/components/add-course-form/add-course-form.component.ts
+++ src/app/components/add-course-form/add-course-form.component.ts
@@ -95,7 +95,7 @@ export class AddCourseFormComponent {
       if (typ == "send")
         this.router.navigate(['/home/courses-page']);
       else {
-        window.location.reload();
+        window.location.replace(window.location.href);
       }
     }, error => {
 
